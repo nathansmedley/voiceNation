ae503eea71e1cefe88d0b35605d98c2d1c1671bf
nathansmedley <67465787+nathansmedley@users.noreply.github.com>
2023-03-13 13:35:30 +0000

    Amends

--- components/hero/Homepage.vue
+++ components/hero/Homepage.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
+    <div v-editable="blok">
+      <h3>
+        {{ blok.name }}
+      </h3>
     </div>
-</template>
-
+  </template>
+   
 <script setup>
+  const storyblokApi = useStoryblokApi()
+  const { data } = await storyblokApi.get('cdn/stories/config', {
+    version: 'draft',
+  })
+    
+  const headerMenu = ref(null)
+  headerMenu.value = data.story.content.header_menu
 </script>
--- components/nav/Default.vue
+++ components/nav/Default.vue
@@ -1,11 +1,26 @@
 <template>
-    <header>
-        <div >
+    <header class="header">
+        <div class="header__nav">
             <NuxtLink to="/">
                 <h1>VoiceNation</h1>
             </NuxtLink>
-            <nav>
+            <nav class="header_nav" aria-labelledby="primary-navigation">
                 <ul>
+                    <li>Solutions</li>
+                    <li>Industries</li>
+                    <li>Pricing</li>
+                    <li>Free Trial</li>
+                    <li>833-456-789</li>
+                </ul>
+            </nav>
+        </div>
+        <div class="header__subnav">
+            <nav aria-labelledby="secondary-navigation">
+                <ul>
+                    <li>Support</li>
+                    <li>Login</li>
+                    <li>About</li>
+                    <li>Resources</li>
                 </ul>
             </nav>
         </div>
@@ -14,3 +29,20 @@
 
 <script setup>
 </script>
+
+<style lang="scss">
+
+.header {
+    width:100%;
+    display:flex;
+
+    &__nav {
+
+    }
+
+    &__subnav {
+
+    }
+}
+
+</style>
